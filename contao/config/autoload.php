0c6077199f5987adeddc3b1fa9e7f3576e9a8c37
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-10-12 20:42:01 +0200

    Add autoload information and templates

---
+++ contao/config/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of cyberspectrum/contao-table-of-contents.
+ *
+ * (c) 2016 CyberSpectrum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    TableOfContents
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2016 CyberSpectrum.
+ * @license    https://github.com/cyberspectrum/contao-table-of-contents/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+TemplateLoader::addFiles([
+    'ce_table-of-contents'      => 'system/modules/table-of-contents/templates',
+    'ce_table-of-contents_list' => 'system/modules/table-of-contents/templates',
+]);
