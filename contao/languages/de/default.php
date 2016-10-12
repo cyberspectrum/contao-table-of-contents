ef9ca979ff308dad8b38d5fa47057cac78e8223c
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-10-12 20:42:27 +0200

    Add dca information and language files

---
+++ contao/languages/de/default.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of cyberspectrum/contao-table-of-contents.
+ *
+ * (c) 2016 CyberSpectrum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    TableOfContents
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2016 CyberSpectrum.
+ * @license    https://github.com/cyberspectrum/contao-table-of-contents/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['CTE']['table-of-contents'] = 'Inhaltsverzeichnis';
